431b23acbc5ea9dc3b747a086bdbf23a1ed40611
DustbinSavesYou <thelonerdude92@gmail.com>
2021-09-25 21:56:59 -0700

    .

---
+++ js/deprecated.js
@@ -0,0 +1,160 @@
+let link01 = document.querySelector('#link01');
+link01.addEventListener('click', function(){
+    let cs = gsap.timeline();
+    cs.to('.col-img img', {
+        duration: 1.2, //2
+        stagger: {
+            from: 1,
+            amount: 0.85  //1.7
+        }, 
+        delay: 0,
+        ease: "power2.out",
+    
+        y: -windowHeight - 10,
+        // opacity: 0,
+    })    
+
+    let img1 = document.querySelectorAll('main .selected img')[0]
+    let img2 = document.querySelectorAll('main .selected img')[1]
+
+    let img = img2
+    // let iot = img.offsetTop, imgHeight = img.offsetHeight, imgcenter = img.offsetHeight / 2, icot = imgcenter + iot, cy = windowHeight / 2
+    // console.log(iot)
+    // let yt = cy - icot
+
+    let iot = img.offsetTop, ih = img.offsetHeight, ihc = ih/2, ioc = iot + ihc;
+    let wh = windowHeight, whc = wh/2, y = whc-ioc;
+    // console.log(`a: ${a}, b: ${b}, c: ${c}, d: ${d}, e: ${e}, f: ${f}, y: ${y}, `)
+    let iol = img.offsetLeft, iw = img.offsetWidth, iw3 = iw * 3, xo = -iol + iw3/2
+
+    cs.to(img, {
+        duration: 0.6,
+        y: y,
+        x: xo,
+        scale: 3,
+    })  
+})
+
+
+
+
+
+
+let link02 = document.querySelector('#link02');
+link02.addEventListener('click', function(){
+    let ls = gsap.timeline();
+    sortedImageList.forEach((img, index) => {
+        let duration, a, b, c, d, e, offsetRight, fluctuation, fluctuation2;
+        fluctuation = Math.random() * (30 - (-30)) - 30;
+        fluctuation2 = Math.random() * (30 - (-30)) - 30;
+        a = 250, b = img.offsetTop;
+        offsetRight = windowWidth - (img.offsetLeft + img.offsetWidth) - a + fluctuation;
+        duration = 1.1;
+        c = (b - a) + fluctuation2;
+        
+        ls.to(img, {
+            duration: duration,
+            y: -c,
+            x: offsetRight,
+            scale: 2,
+            filter: 'saturate(0)',
+            ease: Power2.easeInOut,
+        }, "<")
+    })
+})
+
+
+aboutLink.addEventListener('click', function() {
+    let abtl = gsap.timeline();
+    let duration = 1;
+
+
+    if (about_revealed == false) {
+        about_revealed = true;
+
+        // abtl.to(sortedImageList, {
+        //     duration: 1.5, //2
+        //     stagger: {
+        //         from: 0,
+        //         amount: 0.85  //1.7
+        //     }, 
+        //     delay: 0,
+        //     ease: "power2.out",
+        
+        //     y: -windowHeight - 10,
+        //     opacity: -0.2,
+        // }) 
+
+        abtl.set('#about', {zIndex: 3}, 0)
+        abtl.to('#about', {
+            duration: duration, 
+            opacity: 1,
+            ease: Power3.easeInOut
+        }, 0)
+        abtl.to('nav a', {color: 'white'}, 0)
+        abtl.from('.column-text-inner', {
+            duration: duration + 0.2,
+            y: 310,
+            ease: Power3.easeOut
+        }, 0)
+
+        abtl.to('.main-link', {
+            opacity: 0,
+            display: 'none'
+        }, 0)
+    }
+    else {
+        about_revealed = false;
+        console.log('called')
+
+        abtl.set(sortedImageList, {
+            // duration: 2,
+            // stagger: {
+                // from: 0,
+                // amount: 1.7 // 1.7
+            // }, 
+            // delay: 0,
+            // ease: "power4.out",
+            y: windowHeight/1.4 + 10,
+            opacity: -1,
+        })
+
+        abtl.to('#about', {
+            duration: duration, 
+            opacity: 0,
+            ease: Power3.easeInOut
+        }, 0)
+        abtl.to('nav a', {color: 'black'}, 0)
+        abtl.to('.column-text-inner', {
+            duration: duration + 0.2,
+            y: -310,
+            ease: Power2.easeOut
+        }, 0)
+
+        abtl.to(sortedImageList, {
+            duration: 1.5,
+            stagger: {
+                from: 0,
+                amount: 0.85 // 1.7
+            }, 
+            delay: 0,
+            ease: "power4.out",
+    
+            y: 0,
+            opacity: 1,
+        }, "<")
+
+        abtl.set('#about', {zIndex: -1})
+        abtl.set('.column-text-inner', {
+            y: 0
+        })
+    
+
+        abtl.to('.main-link', {
+            opacity: 1,
+            display: 'block'
+        }, "<")
+
+        
+    }
+})
--- js/main.js
+++ js/main.js
@@ -3,42 +3,51 @@ let tl = gsap.timeline()
 const main = document.querySelector('main');
 const nav = document.querySelector('nav');
 
-// // detect when all thumbnails have been loaded
-// let thumbnails = document.querySelectorAll('main img'), loader = document.querySelector('#loader');
-// // let thumbnailCount = thumbnails.length, loadedCounter = 0;
-// // let percentageIncrease = (100 / thumbnailCount);
-// // let percentage = 0;
+let windowHeight = window.innerHeight, windowWidth = window.innerWidth;
 
-// // thumbnails.forEach(function(thumbnail, index){
-// //     if (thumbnail.complete) {
-// //         incrementCounter()
-// //     } else {
-// //         thumbnail.addEventListener( 'load', incrementCounter, false );
-// //     }
-// // })
+const order = [0,6,13,11,4,16,9,2,1,12,7,14,5,10,3,8,15] // 7 and 14 switched, 14 ^ 7
+let sortedImageList = []
+//create list of all images, run loop through them for serial number, push them into array
+let images = document.querySelectorAll('.col-img img');
+ 
+// detect when all thumbnails have been loaded
+let thumbnails = document.querySelectorAll('main img'), loader = document.querySelector('#loader');
 
-// // function incrementCounter() {
-// //     loadedCounter++
+
+let thumbnailCount = thumbnails.length, loadedCounter = 0;
+let percentageIncrease = (100 / thumbnailCount);
+let percentage = 0;
+
+thumbnails.forEach(function(thumbnail, index){
+    if (thumbnail.complete) {
+        incrementCounter()
+    } else {
+        thumbnail.addEventListener( 'load', incrementCounter, false );
+    }
+})
+
+function incrementCounter() {
+    loadedCounter++
         
-// //     percentage += percentageIncrease;
-// //     document.querySelector('#percentage').innerHTML = `${Math.floor(percentage) + 1}%`;
+    percentage += percentageIncrease;
+    document.querySelector('#percentage').innerHTML = `${Math.floor(percentage) + 1}%`;
 
-// //     if (loadedCounter >= thumbnailCount) {
-// //         // remove loader
-// //         console.log(`${loadedCounter} out of ${thumbnailCount} images loaded`)
+    if (loadedCounter >= thumbnailCount) {
+        // remove loader
+        console.log(`${loadedCounter} out of ${thumbnailCount} images loaded`)
 
-// //         tl.to(loader, {
-// //             duration: 0.3,
-// //             delay: 1,
-// //             opacity: 0,
-// //             display: 'none'
-// //         }, "loaderHide")
-// //     }
-// // }
-// 
+        tl.to(loader, {
+            duration: 0.3,
+            opacity: 0,
+            display: 'none'
+        })
+        executeEntry()
+    }
+}
+
 // let percentage = {percentage: 0}
 // tl.to(percentage, {
-//     duration: 0.5,
+//     duration: .5,
 //     percentage: 100,
 //     ease: "steps(100)",
 //     onUpdate: function(){
@@ -51,96 +60,112 @@ const nav = document.querySelector('nav');
 //     display: 'none'
 // }, "loaderHide")
 
+function executeEntry() {
+    // Images slide in animation
 
-// Images slide in animation
-let windowHeight = window.innerHeight;
-
-tl.from('.col-img img', {
-    duration: 2,
-    stagger: {
-        from: 9,
-        amount: 1.7
-    }, 
-    delay: 0,
-    ease: "power4.out",
-
-    y: windowHeight + 10,
-    opacity: 0,
-})
-
-
-// heading animation
-let headingHeight = document.querySelector('.heading h1').clientHeight,
-headingDelay = 2;
-const Hright = document.querySelector('.heading .right');
-const Hleft = document.querySelector('.heading .left');
-tl.from(Hleft, {
-    duration: 0.6,
-    y: -headingHeight - 0
-}, "<+=2")
-tl.from(Hright, {
-    duration: 0.6,
-    y: +headingHeight + 0
-}, "<")
-
-// change heading text animation function
-function changeHeading(text) {
-    // let text = img.dataset.text;
-    let htl = gsap.timeline();
-    htl.to(Hleft,
-        {
-            duration: 0.6,
-            y: +headingHeight + 0,
-            // ease: Power1.easeInOut
-        })
-    htl.to(Hright,
-        {
-            duration: 0.6,
-            y: -headingHeight - 0,
-            // ease: Power1.easeInOut
-        },
-        "<")
-    
-    
-    htl.set(Hleft,
-        {
-            y: -headingHeight - 0,
-        })
-    htl.set(Hright, {
-            y: +headingHeight + 0,
-        })
-    
-    htl.to(Hleft,
-        {
-            duration: 0.6,
-            y: -17,
-            onStart: function() {
-                Hleft.innerHTML = text;
+    for (let i = 0; i < images.length; i++) {
+        images.forEach(function(img, index) {
+            if (img.dataset.srno == order[i]) {
+                // add it to ordered list
+                sortedImageList.push(img)
             }
         })
-    htl.to(Hright,
-        {
-            duration: 0.6,
-            y: 17, 
-            onStart: function() {
-                Hright.innerHTML = text;
-            }
-        },
-        "<")
+    }
+
+    tl.from(sortedImageList, {
+        duration: 2,
+        stagger: {
+            from: 0,
+            amount: 0.85 // 1.7
+        }, 
+        delay: 0,
+        ease: "power4.out",
+
+        y: windowHeight + 10,
+        opacity: -0.2,
+    })
+
+
+    // heading animation
+    let headingHeight = document.querySelector('.heading h1').clientHeight,
+    headingDelay = 2;
+    const Hright = document.querySelector('.heading .right');
+    const Hleft = document.querySelector('.heading .left');
+    tl.from(Hleft, {
+        duration: 1.1,
+        y: -headingHeight - 0,
+        ease: "power4.out"
+    }, "<+=2")
+    tl.from(Hright, {
+        duration: 1.1,
+        y: +headingHeight + 0,
+        ease: "power4.out"
+    }, "<")
+
+    // change heading text animation function
+    function changeHeading(text) {
+        // let text = img.dataset.text;
+        let htl = gsap.timeline();
+        htl.to(Hleft,
+            {
+                duration: 0.7,
+                y: +headingHeight + 0,
+                ease: "power2.out",
+                // ease: Power1.easeInOut
+            })
+        htl.to(Hright,
+            {
+                duration: 0.7,
+                y: -headingHeight - 0,
+                ease: "power2.out",
+                // ease: Power1.easeInOut
+            },
+            "<")
+        
+        
+        htl.set(Hleft,
+            {
+                y: -headingHeight - 0,
+            })
+        htl.set(Hright, {
+                y: +headingHeight + 0,
+            })
+        
+        htl.to(Hleft,
+            {
+                duration: 0.7,
+                y: -17,
+                ease: "power2.out",
+                onStart: function() {
+                    Hleft.innerHTML = text;
+                }
+            })
+        htl.to(Hright,
+            {
+                duration: 0.7,
+                y: 17, 
+                ease: "power2.out",
+                onStart: function() {
+                    Hright.innerHTML = text;
+                }
+            },
+            "<")
+    }
+
+    //heading text changed everytime mouseover or mouseout is detected
+    let Archive2021 = 'Archive <span>@</span>2021';
+    document.querySelectorAll('.col-img img').forEach((img, index) => {
+        img.addEventListener('mouseover', () => {
+            changeHeading(img.dataset.text)
+        })
+
+        img.addEventListener('mouseout', () => {
+            changeHeading(Archive2021)
+        })
+    })
+
+
 }
-
-//heading text changed everytime mouseover or mouseout is detected
-let Archive2021 = 'Archive @2021';
-document.querySelectorAll('.col-img img').forEach((img, index) => {
-    img.addEventListener('mouseover', () => {
-        changeHeading(img.dataset.text)
-    })
-
-    img.addEventListener('mouseout', () => {
-        changeHeading(Archive2021)
-    })
-})
-
 
 //col-icon is clicked, switch between column background color and greyscale
 let col_icon = document.querySelector('#col-icon');
@@ -153,26 +178,18 @@ col_icon.addEventListener('click', function() {
     }
     if (counter == 0) {
         main.classList.remove('greyscale');
-        main.classList.remove('col-bg');
+        main.classList.remove('col-bg-2');
     }
     if (counter == 1) {
         main.classList.add('greyscale');
-        main.classList.remove('col-bg');
     }
     if (counter == 2) {
-        main.classList.add('greyscale');
         main.classList.add('col-bg');
     }
     if (counter == 3) {
-        main.classList.remove('greyscale');
-        main.classList.add('col-bg');
+        main.classList.add('col-bg-2');
+        main.classList.remove('col-bg');
     }
-    // if (main.classList.contains('greyscale')) {
-    //     main.classList.remove('greyscale')
-    // } 
-    // else {
-    //     main.classList.add('greyscale')
-    // }
 })
 
 
@@ -181,86 +198,82 @@ col_icon.addEventListener('click', function() {
 let about_revealed = false;
 let aboutLink = document.querySelector('#about-link');
 let backgroundColor = 'rgb(250,250,250)', aboutBackgroundColor = 'rgb(0,0,0)';
+let abtl = gsap.timeline();
 aboutLink.addEventListener('click', function() {
-    let abtl = gsap.timeline();
-    let duration = 1;
+    let duration = 0.9;
 
-
+    abtl.play()
     if (about_revealed == false) {
         about_revealed = true;
+        
+        
+        // abtl.set(sortedImageList, {
+            //     y: windowHeight/1.4 + 10,
+            //     opacity: -1,
+            // })
+            abtl.set('.column-text-inner', {
+                y: 0,
+            })
+            
+        abtl.to(sortedImageList, {
+            duration: 1.2,
+            stagger: {
+                from: 16,
+                amount: 0.425 // 1.7
+            }, 
+            delay: 0,
+            ease: Power2.easeIn,
+    
+            y: +windowHeight/1.4 - 10,
+            opacity: -1,
+        })
 
-        abtl.set('#about', {zIndex: 3})
+        abtl.set('#about', {zIndex: 3}, 0)
         abtl.to('#about', {
             duration: duration, 
             opacity: 1,
-            ease: Power3.easeInOut
-        }, 0)
-        abtl.to('nav a', {color: 'white'}, 0)
+            ease: Power2.easeInOut
+        }, 0.25)
+
+        abtl.to('nav a', {color: 'white'}, "<")
         abtl.from('.column-text-inner', {
             duration: duration + 0.2,
             y: 310,
             ease: Power2.easeOut
-        }, 0)
+        }, "<")
 
         abtl.to('.main-link', {
             opacity: 0,
             display: 'none'
-        }, 0)
+        }, "<")
     }
     else {
         about_revealed = false;
-        console.log('called')
-
-        abtl.to('#about', {
-            duration: duration, 
-            opacity: 0,
-            ease: Power3.easeInOut
-        }, 0)
-        abtl.to('nav a', {color: 'black'}, 0)
-        abtl.to('.column-text-inner', {
-            duration: duration + 0.2,
-            y: -310,
-            ease: Power2.easeOut
-        }, 0)
-        abtl.set('#about', {zIndex: -1})
-        abtl.set('.column-text-inner', {
-            y: 0
-        })
-
-        abtl.to('.main-link', {
-            opacity: 1,
-            display: 'block'
-        }, ">")
+        abtl.reverse()
     }
 })
 
 
+
 let link01 = document.querySelector('#link01');
+let cs = gsap.timeline();
 link01.addEventListener('click', function(){
-    let cs = gsap.timeline();
-    // cs.set('main .column', {
-    //     width: 0,
-    //     paddingTop: '15%'
-    // })
-    // cs.to('main .selected', {
-    //     position: 'absolute',
-    //     width: '100%',
-    //     flexFlow: 'row nowrap',
-    //     gap: '3.2vw'
-    // })
-    cs.to('main .unselected', {
-        duration: 0.3,
-        opacity: 0
-    })
-    cs.to('main .selected', {
-        duration: 0.3,
-        opacity: 1,
-    }, "<")
-    let img1 = document.querySelectorAll('main .selected img')[0]
-    let img2 = document.querySelectorAll('main .selected img')[1]
-    cs.to(img1, {
-        y: 30,
-        x: -400,
-        scale: 2
-    })
+    cs.to(sortedImageList, {
+        duration: 1, //2
+        stagger: {
+            from: 0,
+            amount: 0.425  //1.7
+        }, 
+        delay: 0,
+        ease: "power2.out",
+    
+        y: -windowHeight - 10,
+        opacity: -0.2,
+    }) 
+    cs.to('.collection-view', {
+        duration: 0.2,
+        display: 'block',
+        opacity: 1
+    }, ">-0.5")
 })
+
